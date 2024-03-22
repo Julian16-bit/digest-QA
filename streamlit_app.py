be9c0935cc9f5e01b0a4c221fac9c6cebeb84168
Julian16-bit <126834892+Julian16-bit@users.noreply.github.com>
2024-03-22 13:47:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,12 +72,10 @@ if user_input:
   output = completion.choices[0].message
 
   with col1:
-    st.chat_message("You:", user_input)
-    st.chat_message("Bot:", output.content)
-
+    st.write(f"Chatbot answer to: {user_input}")
+    st.write(output.content)
+  
   with col2:
     with st.expander("Click here to see the source"):
       st.write(results)
-
-
 
