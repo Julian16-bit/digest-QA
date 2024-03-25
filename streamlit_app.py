5ad897444b83851ac01bac89196ad3ac5b4eb541
Julian16-bit <126834892+Julian16-bit@users.noreply.github.com>
2024-03-25 15:35:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,7 +77,7 @@ if user_input:
     {"role": "system", "content": "an AI assistant specialized in question-answering tasks, your goal is to offer informative and accurate responses only based on the provided context. If the answer cannot be found within the provided documents, respond with 'I don't have an answer for this question.' Be as concise and polite in your response as possible. "},
     {"role": "user", "content": prompt}
   ],
-    temperature=temperature_selection
+    temperature=temperature_selection,
     top_p=top_p_selection
   )
   
