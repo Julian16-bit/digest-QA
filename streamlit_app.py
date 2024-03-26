9599a89447fbf4c3905b39a9ef22ba3ec6338378
Julian16-bit <126834892+Julian16-bit@users.noreply.github.com>
2024-03-26 10:06:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.markdown("<h1 style='text-align: center; margin-bottom: 100px'>Benefits Q&A C
 st.markdown("""
     <style>
     [data-testid="stSidebar"]{
-    background-color: #4B678A;  
+    background-color: #8C9DB2;  
         }
     </style>
 """, unsafe_allow_html=True)
