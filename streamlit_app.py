cfcd332814ff9945f1d810941078067b2b9ef603
Heng Zhang <54906687+HengZ121@users.noreply.github.com>
2024-04-03 21:29:25 +0000

    French pipeline is finished

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,27 +6,26 @@ from sentence_transformers import SentenceTransformer, CrossEncoder
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from openai import OpenAI
 
-auth_config = weaviate.AuthApiKey(api_key="uokLNfAvageSXij8kUuTlh53DPBz3HMG5Rc5")
-
-client = weaviate.Client(
-  url="https://digest-data-2-hukgw816.weaviate.network",
-  auth_client_secret=auth_config
+auth_config = weaviate.AuthApiKey(api_key="9tqoCx5yjyrLpRAq288U6l3385oOeW22t3vo") 
+ 
+client = weaviate.Client(  
+  url="https://french-digest-co42fmv3.weaviate.network",
+  auth_client_secret=auth_config 
 )
 
 st.set_page_config(layout="wide")
-st.markdown("<h1 style='text-align: center; margin-bottom: 100px'>Benefits Q&A Chat</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; margin-bottom: 100px'>Prestation Q&A Chat</h1>", unsafe_allow_html=True)
 
 with st.sidebar:
-    api_token = st.text_input("Enter your OpenAI API Token:", type='password')
+    api_token = st.text_input("Entrez votre cle API OpenAI ici:", type='password')
     temperature_selection = st.sidebar.slider('Temperature', min_value=0.0, max_value=2.0, value=1.0, step=0.05)
     top_p_selection = st.sidebar.slider('Top_p', min_value=0.0, max_value=1.0, value=1.0, step=0.05)
     # grade_level = st.selectbox('Choose the level of complexity',('elementary school', 'middle school', 'high school', 'college' ))
     # st.write('You selected:', grade_level)
 
 def create_prompt(query):
-  model_name = 'sentence-transformers/all-MiniLM-L6-v2'
-  vect_model = SentenceTransformer(model_name)
-  reranker_model = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
+  vect_model = SentenceTransformer.from_pretrained('camembert-base')
+  reranker_model = SentenceTransformer('antoinelouis/biencoder-electra-base-french-mmarcoFR')
   query1 = query
   query_embedding = vect_model.encode(query1)
   response = (
@@ -49,14 +48,20 @@ def create_prompt(query):
     }
     results.append(result)
 
-  query_doc_pairs = [[query, res["content"]] for res in response["data"]["Get"]["Digest2"]]
+  query_doc_pairs = [[],[]]
+  
+  query_doc_pairs[0] = [query for res in response["data"]["Get"]["Digest2"]]
+  query_doc_pairs[1] = [res["content"] for res in response["data"]["Get"]["Digest2"]]
 
-  scores = reranker_model.predict(query_doc_pairs)
+  q_embeddings = reranker_model.encode(query_doc_pairs[0], normalize_embeddings=True)
+  d_embeddings = reranker_model.encode(query_doc_pairs[1], normalize_embeddings=True)
+
+  scores = (q_embeddings @ d_embeddings.T)[0]
   print(scores)
 
   top_n = 5 ### Cap number of documents that are sent to LLM for RAG
   scores_cp = scores.tolist()
-  documents = [pair[1] for pair in query_doc_pairs]
+  documents = query_doc_pairs[1]
   content = ""
 
   for _ in range(top_n):
@@ -67,10 +72,7 @@ def create_prompt(query):
     del scores_cp[index]
     
   prompt = f"""
-  As an AI assistant specialized in question-answering tasks, your goal is to offer informative and accurate responses
-  based on the provided context. If the answer cannot be found within the provided documents, respond with 'I don't have
-  an answer for this question.' Be as concise and polite in your response as possible, and use simple language. The provided context contains the
-  principles applied in the Employment Insurance (EI) program, and the question is also related to the EI program.
+  En tant qu'assistant IA spécialisé dans les tâches de réponse aux questions, votre objectif est d'offrir des réponses informatives et précises basées sur le contexte fourni. Si la réponse ne peut pas être trouvée dans les documents fournis, répondez par « Je n'ai pas de réponse à cette question ». Soyez aussi concis et poli que possible dans votre réponse et utilisez un langage simple. Le contexte fourni contient les principes appliqués dans le programme d'assurance-emploi (AE), et la question est également liée au programme d'AE.
 
   Context: {content}
   Question: {query}
@@ -97,7 +99,7 @@ if user_input:
   completion = gpt.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "an AI assistant specialized in question-answering tasks, your goal is to offer informative and accurate responses only based on the provided context. If the answer cannot be found within the provided documents, respond with 'I don't have an answer for this question.' Be as concise and polite in your response as possible. "},
+    {"role": "system", "content": "En tant qu'assistant IA spécialisé dans les tâches de réponse aux questions, votre objectif est de proposer des réponses informatives et précises uniquement basées sur le contexte fourni. Si la réponse ne peut pas être trouvée dans les documents fournis, répondez par « Je n'ai pas de réponse à cette question ». Soyez aussi concis et poli que possible dans votre réponse."},
     {"role": "user", "content": prompt}
   ],
     temperature=temperature_selection,
